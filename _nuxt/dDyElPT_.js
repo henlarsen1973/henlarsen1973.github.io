13173c779d4d886f3954c62345e425e1901b45ae
Henning Larsen <henlarsen1973@gmail.com>
2024-04-14 09:04:14 +0200

    Github preset

---
+++ _nuxt/dDyElPT_.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{v as o,t}from"./3-csd2gp.js";const r={name:"MyButton"};function a(n,s,c,p,_,d){return t(),o("h1",null,"Work in progress")}const m=e(r,[["render",a],["__scopeId","data-v-a5a8a1e2"]]);export{m as default};
